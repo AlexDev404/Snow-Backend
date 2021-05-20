a6bd8d6abf8902839fd9c6df89e627e9e1b1be9f
Immanuel Garcia <34188635+AlexDev404@users.noreply.github.com>
2021-05-20 17:36:11 -0600

    idk

--- routes/oauth.js
+++ routes/oauth.js
@@ -8,21 +8,21 @@ module.exports = (app) => {
         username = req.body.email.split("@")[0];  
       }
       res.json({
-        "access_token" : "SnowToken",
-        "account_id" : username || "SnowUser",
-        "app" : "fortnite",
-        "client_id" : "clientid",
-        "client_service" : "fortnite",
-        "device_id" : "deviceid",
-        "displayName" : username || "SnowUser",
-        "expires_at" : "9999-12-02T01:12:00.159Z",
-        "expires_in" : 28800,
-        "in_app_id" : username || "SnowUser",
-        "internal_client" : true,
-        "refresh_expires" : 90000,
-        "refresh_expires_at" : "9999-12-02T01:12:00.159Z",
-        "refresh_token" : "SnowToken",
-        "token_type" : "bearer"
+        access_token: "SnowToken",
+        account_id: username || "SnowUser",
+        app: "fortnite",
+        client_id: "clientid",
+        client_service: "fortnite",
+        device_id: "deviceid",
+        displayName: username || "SnowUser",
+        expires_at: "9999-12-02T01:12:00.159Z",
+        expires_in: 28800,
+        in_app_id: username || "SnowUser",
+        internal_client: true,
+        refresh_expires: 90000,
+        refresh_expires_at: "9999-12-02T01:12:00.159Z",
+        refresh_token: "SnowToken",
+        token_type: "bearer"
       })
     app.get("/account/api/oauth/verify"), (req, res) => {
        res.status(200)
